3777a648a9b32a5e5c96de06d7a55a92da579de6
Avinash Sekar <78691324+avin045@users.noreply.github.com>
2022-07-28 14:58:52 +0000

    ascending

--- array_ascending.py
+++ array_ascending.py
@@ -1,1 +1,27 @@
-print('hi')
+arr = list(map(int,input("Enter list : ").split()));
+#       0 1  2  3  4 5
+len_arr = len(arr); # 6
+
+
+# Traverse
+for i in range(len_arr):
+  for j in range(i+1,len_arr):
+    if arr[i] > arr[j]:
+      arr[i],arr[j] = arr[j],arr[i]
+print(arr)
+
+'''
+1. compare
+2. if arr[0] > arr[1]
+      arr[0],arr[1] = arr[1],arr[0]
+      47      1          1    47
+
+      i = 0  :  j = 1
+      47 = 1
+      47 = 10
+      47 = 25
+      47 = 19
+      47 = 2
+
+'''
+  # 47 1 10 25 19 2
---
+++ main.py
@@ -0,0 +1,6 @@
+hi = 'maddy'
+num = '2'
+n = '9.0'
+print(type(num))
+a = ['1','3','67']
+print(list(map(float,a)))
